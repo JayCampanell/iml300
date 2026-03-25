9580cc1e0dfc332c8e46cc2171cd70fe35a23bca
Jay Campanell <jaymcampanell@gmail.com>
2026-03-24 17:04:58 -0700

    update to website

--- made-by-humans/click.js
+++ made-by-humans/click.js
@@ -1,3 +1,5 @@
+ // GENERATED WITH GEMINI
+ 
  var click = $("#click")[0];
     $(".center a").click(function(event) {
         event.preventDefault(); 
@@ -44,3 +46,18 @@
             window.location.href = targetUrl;
         }, 300);
     });
+    $(".collage-container a").click(function(event) {
+    event.preventDefault(); 
+    
+    // Get the URL the link is supposed to go to
+    var targetUrl = $(this).attr("href");
+    
+    // Play the sound
+    click.play();
+    
+    // 4. Wait a fraction of a second, then navigate to the new page.
+    // (500 milliseconds = half a second. Adjust this based on how long your mp3 is!)
+    setTimeout(function() {
+        window.location.href = targetUrl;
+    }, 300);
+});
